2f26f1c7e638042ef915fcd6fcc9c2120f9430b8
tianfan-wkb <910520150@qq.com>
2019-07-02 17:27:25 +0800

    chore

--- src/components/guide/index.vue
+++ src/components/guide/index.vue
@@ -14,16 +14,16 @@
         <em style="transform: translate(7px,3px)rotate(90deg) scale(1,2)" />
       </div>
     </div>
-    <div class="guide left">
-      <!-- <p>
+    <!-- <div class="guide left">
+      <p>
         <a href="https://github.com/Binaryify/vue-tetris" rel="noopener noreferrer" target="_blank" v-bind="{title:linkTitle}">{{github}}:</a>
         <br />
         <iframe src="https://ghbtns.com/github-btn.html?user=Binaryify&repo=vue-tetris&type=star&count=true" frameBorder="0" scrolling="0" width="170px" height="20px" style="transform: scale(1.68);transform-origin: center left" />
         <br />
         <iframe src="https://ghbtns.com/github-btn.html?user=Binaryify&repo=vue-tetris&type=fork&count=true" frameBorder="0" scrolling="0" width="170px" height="20px" style="transform: scale(1.68);transform-origin: center left" />
-      </p> -->
+      </p>
       <div class="space">SPACE</div>
-    </div>
+    </div> -->
     <!-- <div class="guide qr">
       <img :src="QRSrc" v-bind="{title:QRTitle,alt:QRCode}" />
     </div> -->
